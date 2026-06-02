5c878804180d19cc9268a6455f0c9af446fccabf
kelson8 <kelson@kelsoncraft.net>
2026-06-01 21:34:02 -0400

    Move displaying page lists into new function
    * I created display-page-list.tsx to display a list of pages on the website from a json file, this should help with making more of these in the future.

--- src/app/misc/page.tsx
+++ src/app/misc/page.tsx
@@ -1,32 +1,19 @@
 import React from 'react'
 import {MainContainer} from "@/components/main-container";
-import {blueLinkCss, containerPageClass, miscPagesJsonFile} from "@/util/constants";
-import {TestForm} from "@/components/test-form";
-import Link from "next/link";
-import {ReadWebpageJsonFile} from "@/components/read-json-files";
+import DisplayPageList from "@/components/display-page-list";
+import {miscPagesJsonFile} from "@/util/constants";
 
 // Misc pages
 
 // The page list gets generated from the miscPagesJsonFile that is setup in constants.ts
 
 const MiscPage = () => {
-
-    //ReadWebpageJsonFile
     return (
         <div>
             <MainContainer>
-                <br></br>
-                <h1 className="text-4xl text-center text-indigo-500 text-bold">Misc</h1>
-                <br></br>
-
-                <div className={containerPageClass}>
-                    <p className="text-center">Misc pages for this website</p>
-
-                    {ReadWebpageJsonFile(miscPagesJsonFile)}
-                </div>
+                {DisplayPageList("Misc", "Misc pages for this website", miscPagesJsonFile)}
             </MainContainer>
         </div>
-
     );
 }
 
---
+++ src/components/display-page-list.tsx
@@ -0,0 +1,25 @@
+import {containerPageClass} from "@/util/constants";
+import {ReadWebpageJsonFile} from "@/components/read-json-files";
+import React from "react";
+
+/**
+ * Basic function to display a website page list from my json files.
+ * @param pageHeader The header for the page.
+ * @param pageDescription The description, shown under the header.
+ * @param jsonFile The json file to load the page list info from.
+ */
+export default function DisplayPageList(pageHeader: string, pageDescription: string, jsonFile: string) {
+    return (
+        <>
+            <br></br>
+            <h1 className="text-4xl text-center text-indigo-500 text-bold">{pageHeader}</h1>
+            <br></br>
+
+            <div className={containerPageClass}>
+                <p className="text-center">{pageDescription}</p>
+
+                {ReadWebpageJsonFile(jsonFile)}
+            </div>
+        </>
+    )
+}
